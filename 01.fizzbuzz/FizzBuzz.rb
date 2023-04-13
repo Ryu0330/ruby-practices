da9c3b47d09848168e56c4216eec7f45e26617f3
Ryu0330 <aoiryu33@gmail.com>
2023-04-13 22:35:17 +0900

    コメント頂いた箇所の修正

--- 01.fizzbuzz/FizzBuzz.rb
+++ 01.fizzbuzz/FizzBuzz.rb
@@ -1,13 +1,11 @@
-21.times do |x|
-    next if x==0
-    
-    if x%5==0 && x%3==0 then
-        puts "FizzBuzz"
-    elsif x%5==0 then
-        puts "Buzz"
-    elsif x%3==0 then
-        puts "Fizz"
-    else 
-        puts x
-    end
+1.upto(20) do |i|
+  if i % 5 == 0 && i % 3 == 0 then
+    puts "FizzBuzz"
+  elsif i % 5 == 0 then
+    puts "Buzz"
+  elsif i % 3 == 0 then
+    puts "Fizz"
+  else 
+    puts i
+  end
 end
