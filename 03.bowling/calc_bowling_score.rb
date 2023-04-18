b6d80789a8503930911cb43e55c9e2f736c83df7
Ryu0330 <aoiryu33@gmail.com>
2023-04-19 00:34:44 +0900

    一投目の処理を少し追加

--- 03.bowling/calc_bowling_score.rb
+++ 03.bowling/calc_bowling_score.rb
@@ -4,3 +4,20 @@ input_array.map!(&:to_i)
 p input_array
 
 # boolean変数でストライクとスペアかその他を判定して、trueならストライクかどうかで二投目の判定
+is_first_throw = True
+count_frame = 0
+is_knocked_down_all = False
+
+input_array.each do |score|
+  count_frame += 1 if is_first_throw
+  if count_frame == 1 && is_first_throw = True
+    score_this_frame = score
+    is_first_throw = False
+    next
+  end
+
+  if count_frame = 10
+
+  end 
+
+puts result
