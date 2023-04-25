1d9d529373419707e1cb41acde988f22462650db
Ryu0330 <aoiryu33@gmail.com>
2023-04-25 19:27:16 +0900

    実行時の日付が1日の場合の不具合の修正、ファイル名に拡張子を追加

--- 02.calendar/calendar_command.rb
+++ 02.calendar/calendar_command.rb
@@ -30,12 +30,15 @@ puts "#{first_date.month}月 #{first_date.year}".center(20)
 puts "日 月 火 水 木 金 土"
 
 output_day = ""
+if not first_date.sunday?
+  output_day += "  " + "   " * (first_date.wday - 1)
+end
 
 (first_date..last_date).each do |date|
   output_day += format_date(date)
 
   if date.saturday?
-    output_day = output_day.rjust(20) + "\n"
+    output_day += "\n"
   end
 end
 
