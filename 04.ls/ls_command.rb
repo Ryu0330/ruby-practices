5de0fa35d5ce3e50143af8c31e626b70ce7ca5aa
Ryu0330 <aoiryu33@gmail.com>
2023-06-04 14:35:29 +0900

    レビューに応じた修正

--- 04.ls/ls_command.rb
+++ 04.ls/ls_command.rb
@@ -1,47 +1,38 @@
 # frozen_string_literal: true
 
-COLUMN_WIDTH = 3
+COLUMN_COUNT = 3
 
 def main
-  files_array = fetch_files_array
-  puts format_to_show(files_array)
+  files = fetch_files
+  puts format_to_show(files)
 end
 
-def fetch_files_array
-  files_array = []
-  dir_path = configure_dir
-  Dir.foreach(dir_path) do |file|
-    next if file.start_with?('.')
-
-    files_array	<< file
-  end
-  files_array
+def fetch_files
+  Dir.foreach(dir_path).filter_map { |file| file unless file.start_with?('.') }
 end
 
-def configure_dir
-  ARGV[0] ||= '.'
+def dir_path
+  ARGV[0] || '.'
 end
 
-def format_to_show(files_array)
-  files_array.sort!
+def format_to_show(files)
+  sorted_files = files.sort
 
-  row_count = if (files_array.size % COLUMN_WIDTH).zero?
-                files_array.size / COLUMN_WIDTH
-              else
-                files_array.size / COLUMN_WIDTH + 1
-              end
+  row_count = (files.size / COLUMN_COUNT.to_f).ceil
 
-  longest_number_among_filename = files_array.max_by(&:length).length
+  column_width = sorted_files.max_by(&:length).length
 
-  formatted_string = ''
+  formatted_strings = []
   row_count.times do |row|
-    COLUMN_WIDTH.times do |col|
-      formatted_string += files_array[row + col * row_count].to_s.ljust(longest_number_among_filename + 3)
+    same_row_files = []
+    COLUMN_COUNT.times do |col|
+      target_filename = sorted_files[row + col * row_count].to_s
+      same_row_files << target_filename.ljust(column_width + 3)
     end
-    formatted_string += "\n"
+    formatted_strings << same_row_files.join
   end
 
-  formatted_string
+  formatted_strings.join("\n")
 end
 
 main
