bbd69cf8c91eb249303227c357a79aed26f878c9
Ryu0330 <aoiryu33@gmail.com>
2023-06-16 17:29:31 +0900

    aオプションの登録～処理の分岐まで実装(aオプションの処理自体は未)

--- 04.ls/ls_command.rb
+++ 04.ls/ls_command.rb
@@ -4,35 +4,33 @@ require 'optparse'
 COLUMN_COUNT = 3
 
 def main
+  opt = OptionParser.new
+  is_a_option = false
+  opt.on('-a') { |v| is_a_option = v}
+  opt.parse!(ARGV)
+  p is_a_option
   if is_a_option
-    files = fetch_files
+    p 'a option is true' 
+    files = fetch_files(option: 'a')
   else
     files = fetch_files
   end
   puts format_to_show(files)
 end
 
-def is_a_option
-  input_option = analyze_argv
-  p input_option[:]
-  input_option[:]
-end
-
-def fetch_files
-  Dir.foreach(dir_path).filter_map { |file| file unless file.start_with?('.') }
+def fetch_files(option: '')
+ case option
+  when '' then
+    Dir.foreach(dir_path).filter_map { |file| file unless file.start_with?('.') }
+  when 'a' then
+    p ARGV
+    p Dir.foreach(dir_path).each
+    Dir.foreach(dir_path).each
+  end
 end
 
 def dir_path
   ARGV[0] || '.'
-end
-
-def analyze_argv
-  opt = OptionParser.new
-  input = {}
-  opt.on('-') {|v| input[path:] = v}
-  opt.on('-a') {|v| input[a:] = v}
-
-  opt.parse!(ARGV)
 end
 
 def format_to_show(files)
